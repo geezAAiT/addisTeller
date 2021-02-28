2394a2a8fcdd6d4444022060a48606990c31819f
Fikir <fikirmuluken@gmail.com>
2021-02-28 20:12:25 +0300

    post feature

--- lib/post/bloc/post_event.dart
+++ lib/post/bloc/post_event.dart
@@ -1,55 +1,55 @@
-// import 'package:equatable/equatable.dart';
-// import '../post.dart';
+import 'package:equatable/equatable.dart';
+import '../post.dart';
 
-// abstract class PostEvent extends Equatable {
-//   const PostEvent();
-// }
+abstract class PostEvent extends Equatable {
+  const PostEvent();
+}
 
-// class PostLoad extends PostEvent {
-//   const PostLoad();
+class PostLoad extends PostEvent {
+  const PostLoad();
 
-//   @override
-//   List<Object> get props => [];
-// }
+  @override
+  List<Object> get props => [];
+}
 
-// class StationPostLoad extends PostEvent {
-//   const StationPostLoad({this.stationID});
-//   final String stationID;
-//   @override
-//   List<Object> get props => [stationID];
-// }
+class StationPostLoad extends PostEvent {
+  const StationPostLoad({this.stationID});
+  final String stationID;
+  @override
+  List<Object> get props => [stationID];
+}
 
-// class PostCreate extends PostEvent {
-//   final Post post;
-//   final String stationID;
-//   const PostCreate(this.post, this.stationID);
+class PostCreate extends PostEvent {
+  final Post post;
+  final String stationID;
+  const PostCreate(this.post, this.stationID);
 
-//   @override
-//   List<Object> get props => [post];
+  @override
+  List<Object> get props => [post];
 
-//   @override
-//   String toString() => 'Post Created {post: $post}';
-// }
+  @override
+  String toString() => 'Post Created {post: $post}';
+}
 
-// class PostUpdate extends PostEvent {
-//   final Post post;
-//   const PostUpdate(this.post);
+class PostUpdate extends PostEvent {
+  final Post post;
+  const PostUpdate(this.post);
 
-//   @override
-//   List<Object> get props => [post];
+  @override
+  List<Object> get props => [post];
 
-//   @override
-//   String toString() => 'Post Updated {post: $post}';
-// }
+  @override
+  String toString() => 'Post Updated {post: $post}';
+}
 
-// class PostDelete extends PostEvent {
-//   final Post post;
-//   final String stationID;
-//   const PostDelete({this.post, this.stationID});
+class PostDelete extends PostEvent {
+  final Post post;
+  final String stationID;
+  const PostDelete({this.post, this.stationID});
 
-//   @override
-//   List<Object> get props => [post, stationID];
+  @override
+  List<Object> get props => [post, stationID];
 
-//   @override
-//   toString() => 'Post Deleted {post: $post}';
-// }
+  @override
+  toString() => 'Post Deleted {post: $post}';
+}
